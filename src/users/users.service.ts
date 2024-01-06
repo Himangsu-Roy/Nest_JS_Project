e054e64a82532ca2a9232970defb35ecd10f6ac7
himangsu roy <himur98@gmail.com>
2024-01-06 09:39:56 +0600

    added logic about create, find, update, delete

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,86 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    {
+      id: 1,
+      name: 'John Doe',
+      email: 'john@gmail.com',
+      role: 'INTERN',
+    },
+    {
+      id: 2,
+      name: 'Jane Doe',
+      email: 'jane@gmail.com',
+      role: 'ADMIN',
+    },
+    {
+      id: 3,
+      name: 'John Smith',
+      email: 'smith@gmail.com',
+      role: 'ENGINEER',
+    },
+    {
+      id: 4,
+      name: 'Jane Smith',
+      email: 'janesmith@gmail.com',
+      role: 'INTERN',
+    },
+    {
+      id: 5,
+      name: 'John Doe',
+      email: 'doe@gmail.com',
+      role: 'ENGINEER',
+    },
+  ];
+
+  findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    if (role) {
+      return this.users.filter((user) => user.role === role);
+    }
+    return this.users;
+  }
+
+  findOne(id: number) {
+    const user = this.users.find((user) => user.id === id);
+    return user;
+  }
+
+  create(user: {
+    name: string;
+    email: string;
+    role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+  }) {
+    const usersByHighestId = [...this.users].sort((a, b) => b.id - a.id);
+    const newUser = {
+      id: usersByHighestId[0].id + 1,
+      ...user,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(
+    id: number,
+    updateUser: {
+      name?: string;
+      email?: string;
+      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    this.users = this.users.map((user) => {
+      if (user.id === id) {
+        return { ...user, ...updateUser };
+      }
+      return user;
+    });
+    return this.findOne(id);
+  }
+
+  delete(id: number) {
+    const removeUser = this.findOne(id);
+    this.users = this.users.filter((user) => user.id !== id);
+    return removeUser;
+  }
+}
