41e1f6136822460413e10eeec6cbe0122cbb84dd
himangsu roy <himur98@gmail.com>
2024-01-06 12:04:42 +0600

    added dto of createuser and update user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,16 +4,20 @@ import {
   Delete,
   Get,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
   Query,
 } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   @Get() // GET /users
   findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-    return [];
+    return this.usersService.findAll(role);
   }
 
   //   @Get('interns')
@@ -21,23 +25,39 @@ export class UsersController {
   //     return [];
   //   }
 
+  // StringValidationPipe, NumberValidationPipe, PositiveNumberValidationPipe, PositiveNumberPipe, PositiveNumberPipe, PositiveNumberPipe, PositiveNumberPipe, PositiveNumberPipe, PositiveNumberPipe, PositiveNumberPipe, PositiveNumberPipe, PositiveNumberPipe, PositiveNumberPipe, PositiveNumberPipe, PositiveNumberPipe,
   @Get(':id') // GET /users/:id
-  findOne(@Param('id') id: string) {
-    return { id };
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.findOne(id);
   }
 
   @Post() // POST /users
-  create(@Body() user: {}) {
-    return user;
+  create(
+    @Body()
+    user: {
+      name: string;
+      email: string;
+      role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.usersService.create(user);
   }
 
   @Patch(':id') // PATCH /users/:id
-  update(@Param('id') id: string, @Body() userUpdate: {}) {
-    return { id, ...userUpdate };
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body()
+    userUpdate: {
+      name?: string;
+      email?: string;
+      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.usersService.update(id, userUpdate);
   }
 
   @Delete(':id') // DELETE /users/:id
-  delete(@Param('id') id: string) {
-    return { id };
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.delete(id);
   }
 }
